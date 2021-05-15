662883e54af3e5d3a0be7ce368cfb5dba7fbf55e
Rustem Ilialov <ilialovrustem@gmail.com>
2021-05-15 20:41:35 +0300

    Fixed new function in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,8 +8,7 @@ public class Main {
     public static void main(String[] args) {
         String name = "";
         System.out.println("Hello! What is your name?");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             name = reader.readLine();
         } catch (IOException e) {
             e.printStackTrace();
