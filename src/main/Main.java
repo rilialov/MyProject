974364cd19fc6721b71b11e29e9225450a9ce2ea
Rustem Ilialov <ilialovrustem@gmail.com>
2021-05-15 20:35:50 +0300

    Added new function in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String name = "";
+        System.out.println("Hello! What is your name?");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            name = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello, " + name + "!");
     }
 }
