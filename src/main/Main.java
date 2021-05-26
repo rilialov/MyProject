350dd7255d1194f3cb96f4f8e1b91f4671f2fff5
rilialov <ilialovrustem@gmail.com>
2021-05-26 15:53:18 +0300

    added new fields and methods: getAnswers() and print()

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,12 @@ import java.io.InputStreamReader;
 public class Main {
     private static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
 
+    private static String name, lastname;
+    private static String phone, email;
+
     public static void main(String[] args) {
-        System.out.println("Hello! What is your name?");
-        String name = read();
-        System.out.println("Hello, " + name + "!");
+        getAnswers();
+        print();
     }
 
     private static String read() {
@@ -21,5 +23,21 @@ public class Main {
             System.err.println("Error: problems in read()");
         }
         return s;
+    }
+
+    private static void getAnswers() {
+        System.out.println("What is your name?");
+        name = read();
+        System.out.println("Your last name?");
+        lastname = read();
+        System.out.println("Phone number?");
+        phone = read();
+        System.out.println("E-mail?");
+        email = read();
+    }
+
+    private static void print() {
+        System.out.printf("Hello, %s %s! Nice to meet you!%n", lastname, name);
+        System.out.printf("Your contacts: phone - %s, e-mail - %s%n", phone, email);
     }
 }
