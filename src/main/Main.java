76782520c85ff151df08b38cee82ef1c20780e23
rilialov <ilialovrustem@gmail.com>
2021-05-27 14:57:41 +0300

    added new method readFromXML() and logic in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.*;
 import java.util.Properties;
 
@@ -12,22 +14,37 @@ public class Main {
     private static String name, lastname;
     private static String phone, email;
     private static String course, trainer, date;
+    private static Form form;
 
     private static final String XML_OUT = "C:\\Java\\form.xml";
+    private static final String XML_IN = "C:\\Java\\info.xml";
 
     public static void main(String[] args) {
-        getAnswers();
-        loadProperties();
-        print();
-        Form form = new Form(course, trainer, date, name, lastname, phone, email);
-        saveToXML(form);
-        close();
+        System.out.println("Hello! Do you want to answer(a) questions or load from file(f)?");
+        String s = read();
+        switch (s) {
+            case ("f") -> {
+                readFromXML();
+                print();
+            }
+            case ("a") -> {
+                getAnswers();
+                loadProperties();
+                print();
+                form = new Form(course, trainer, date, name, lastname, phone, email);
+                saveToXML(form);
+                close();
+            }
+            default -> System.out.println("bye!");
+        }
     }
 
     private static String read() {
         String s = "";
         try {
-            s = BR.readLine();
+            do {
+                s = BR.readLine();
+            } while(s.equals(""));
         } catch (IOException e) {
             System.err.println("Error: problems in read()");
         }
@@ -84,5 +101,22 @@ public class Main {
         } catch (Exception e) {
             System.err.println("Error: problems in saveToXML()");
         }
+    }
+
+    private static void readFromXML() {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Form.class);
+            Unmarshaller un = jaxbContext.createUnmarshaller();
+            form = (Form) un.unmarshal(new File(XML_IN));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        course = form.getCourse();
+        trainer = form.getTrainer();
+        date = form.getDate();
+        name = form.getName();
+        lastname = form.getLastname();
+        phone = form.getPhone();
+        email = form.getEmail();
     }
 }
