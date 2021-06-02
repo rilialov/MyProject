76e734c132432c973106d701042a596a4869aada
rilialov <ilialovrustem@gmail.com>
2021-06-02 16:12:59 +0300

    refactor for making executable jar

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -7,6 +7,10 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
---
+++ src/main/java/MainLauncher.java
@@ -0,0 +1,6 @@
+public class MainLauncher {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
